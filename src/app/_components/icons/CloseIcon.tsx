31b69a2f5f88edc16230439aa17be0462cc303a6
Ingo Knorr <ingoknorr@gmail.com>
2025-03-11 14:07:57 +0100

    added storway favicons

--- src/app/_components/icons/CloseIcon.tsx
+++ src/app/_components/icons/CloseIcon.tsx
@@ -9,9 +9,9 @@ export const CloseIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-x"
     >
       <path d="M18 6 6 18" />
--- src/app/_components/icons/MenuIcon.tsx
+++ src/app/_components/icons/MenuIcon.tsx
@@ -10,9 +10,9 @@ export const MenuIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="lucide lucide-menu"
     >
       <line x1="4" x2="20" y1="12" y2="12"></line>
