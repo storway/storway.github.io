d51fb9b7999932ecc86c78d58013368083780dde
Ingo Knorr <ingoknorr@gmail.com>
2025-03-12 16:49:04 +0100

    changes size of shield icon to work properly on mobile

--- src/app/_components/icons/ShieldIcon.tsx
+++ src/app/_components/icons/ShieldIcon.tsx
@@ -5,7 +5,7 @@ export const ShieldIcon = () => {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             version="1.1"
-            width="512px"
+            width="259x"
             height="259px"
             style={{
                 shapeRendering: 'geometricPrecision',
