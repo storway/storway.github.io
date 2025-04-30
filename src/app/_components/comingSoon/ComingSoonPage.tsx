cd986647e9261bd303bc940de3272bd7efbab567
Ingo Knorr <ingoknorr@gmail.com>
2025-04-30 10:41:30 +0200

    changes to comming soon page

--- src/app/_components/comingSoon/ComingSoonPage.tsx
+++ src/app/_components/comingSoon/ComingSoonPage.tsx
@@ -4,12 +4,12 @@ import { StorwayLogo } from '../icons/StorwayLogo'
 export const ComingSoonPage = () => {
     return (
         <>
-            <div className="flex h-screen w-full flex-col items-center justify-center bg-white">
-                <div className="flex items-end justify-center">
+            <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-white">
+                {/* <div className="absolute bottom-[-4600px] left-1/2 h-[2700px] w-[2700px] -translate-x-1/2 -translate-y-3/4 transform rounded-full bg-gray-200" /> */}
+                <div className="z-10 flex flex-col items-center justify-center">
                     <StorwayLogo className="w-32 sm:w-60" />
-                    <h1 className="text-6xl sm:text-8xl">Storway</h1>
+                    <p className="text-2xl sm:text-4xl">Coming soon.</p>
                 </div>
-                <p className="text-2xl sm:text-4xl">Coming soon.</p>
             </div>
         </>
     )
